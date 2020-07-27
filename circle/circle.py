a6afa926ab949cc8e5f000577cb7a630035d117f
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2020-07-27 11:07:37 -0400

    working on csv exercise

--- circle/circle.py
+++ circle/circle.py
@@ -2,44 +2,44 @@ from math import pi
 
 class Circle:
 	def __init__(self, r=1):
-		if r >= 0:
-			self._radius = r
-		else:
-			raise ValueError("radius cannot be negative")
+		self.radius = r
+
 
 	@property
 	def radius(self):
 		return self._radius
 
+
 	@radius.setter		
 	def radius(self, val):
 		if val < 0:
 			raise ValueError("radius cannot be negative")
-		else:
-			self._radius = val
-			return self._radius
+		self._radius = val
+			
 
+	# we don't need to check if the diameter is negative because it calls the radius property setter	
 	@property  # lets a method be accessed as an attribute instead of a method
 	def diameter(self):
 		return self.radius * 2
 
+
 	@diameter.setter  # allows you to work backwards
 	def diameter(self, val):
-		if val >= 0:
-			self.radius = val / 2
-		else:
-			raise ValueError("Diameter cannot be negative.")
+		self.radius = val / 2
 		
+
 	@property
 	def area(self):
 		return pi * self.radius ** 2
 	
-	@area.setter
-	def area(self, val):
-		raise AttributeError("Can't set the area.")
+	# we don't need this. by trying to set the property, it will raise an error
+	# @area.setter
+	# def area(self, val):
+	# 	raise AttributeError("Can't set the area.")
 
 	def __repr__(self):
 		return f'Circle({self.radius})'
+
 
 if __name__ == '__main__':
 	c = Circle()
