c1538872dd84a7950ee0b61f10cbbc98ccd9ff66
Grant Forsythe <grantwforsythe@gmail.com>
2021-05-27 14:57:29 -0400

    Add solution for orderset.py

--- ordered_set/orderedset.py
+++ ordered_set/orderedset.py
@@ -1,14 +1,38 @@
+from collections.abc import MutableSet, Sequence
 
-class OrderedSet:
-	def __init__(self, args):
-		self.args = []
-		for arg in args:
-			if arg not in self.args:
-				self.args.append(arg)
 
-	def __iter__(self):
-		for val in sorted(self.args):
-			yield val
+class OrderedSet(Sequence, MutableSet):
 
-	def __contains__(self, key):  # allows for the use of in on the object
-		return key in self.args
+    """Set-like object that maintains insertion order of items."""
+
+    def __init__(self, iterable):
+        self.items = set()
+        self.order = []
+        self |= iterable
+
+    def __contains__(self, item):
+        return item in self.items
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, index):
+        return self.order[index]
+
+    def add(self, item):
+        if item not in self.items:
+            self.order.append(item)
+        self.items.add(item)
+
+    def discard(self, item):
+        if item in self.items:
+            self.order.remove(item)
+            self.items.remove(item)
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return (
+                len(self) == len(other) and
+                all(x == y for x, y in zip(self, other))
+            )
+        return super().__eq__(other)from collections.abc import MutableSet
